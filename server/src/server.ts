eb6bd56e65ff4c6986f0ff8aa19bf920d8a090b7
Patrickwds <patrickwds1@gmail.com>
2023-01-24 23:51:13 -0300

    Isolate Backend

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,21 +1,14 @@
 import Fastify from 'fastify';
 import cors from '@fastify/cors'
-import { PrismaClient } from '@prisma/client'
+import { appRoutes } from './routes';
 
 const app = Fastify()
-const prisma = new PrismaClient()
 
 app.register(cors)
-
+app.register(appRoutes)
 /**
  * Metodo HTTP: GET, Post Put, Patch, Delete
  */
-
-app.get('/hello', async () => {
-    const habits = await prisma.habit.findMany()
-
-    return habits
-})
 
 app.listen({
     port: 3333,
