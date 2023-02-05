207458c52e63ff9fb6977ce69261aa3fb6b71c32
Patrickwds <patrickwds1@gmail.com>
2023-02-05 20:11:14 -0300

    Mobile HabitDay

--- mobile/src/components/Header.tsx
+++ mobile/src/components/Header.tsx
@@ -7,7 +7,7 @@ import Logo from '../assets/logo.svg';
 export function Header() {
     return (
         <View className="w-full flex-row items-center justify-between">
-            <Logo />
+            
 
             <TouchableOpacity
                 activeOpacity={0.7}
