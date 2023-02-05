4714797ba81b25f2bc517238b679f0beb919a865
Patrickwds <patrickwds1@gmail.com>
2023-02-05 20:45:53 -0300

    Fix: Mobile Header logo metro config, button itens center

--- mobile/src/components/Header.tsx
+++ mobile/src/components/Header.tsx
@@ -7,11 +7,11 @@ import Logo from '../assets/logo.svg';
 export function Header() {
     return (
         <View className="w-full flex-row items-center justify-between">
-            
+            <Logo />
 
             <TouchableOpacity
                 activeOpacity={0.7}
-                className="flex-row h-11 px-4 border border-violet-500 rounded-lg itens-center"
+                className="flex-row h-11 px-4 border border-violet-500 rounded-lg items-center"
             >
                 <Feather
                     name="plus"
